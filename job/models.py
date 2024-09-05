62bfd23f14363cf290960094e418b5d2ac569bf6
ahmedabougabal <ahmedabougabal@live.com>
2024-09-05 23:14:58 +0300

    Sqlite is now linked and reading the job models, admin dashboard is working as expected and jobs can be added

--- job/models.py
+++ job/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+JOB_TYPES = (
+  ('Full Time','Full Time'),
+  ('Part Time','Part Time'),
+)
+
 # Create your models here.
+class Job(models.Model):
+  title = models.CharField(max_length=100)
+  # location
+  # job (may be either a full-time or part-time) => we use choices
+  job_type = models.CharField(max_length=15, choices=JOB_TYPES)
+  description = models.TextField(max_length=1000)
+  published_at = models.DateTimeField(auto_now_add=True)
+  vacancies=models.IntegerField(default=1)
+  salary = models.IntegerField(default=0)
+  experience = models.IntegerField(default=1)
+
+  #since i need to rename the job added on my admin dashboard
+  # to a name of my choice, i have to use def __str__(self)
+  def __str__(self):
+    return self.title
