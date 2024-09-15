46b0973981162a82d262f4f5bdb696b0b0ce7855
ahmedabougabal <ahmedabougabal@live.com>
2024-09-15 13:02:00 +0300

    feat: creates 2 templates inside the job app implements the job_list view function that returns the whole jobs listed implements the job_detail for accessing a specific job from the jobs listed using job_id

--- job/views.py
+++ job/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from multiprocessing.pool import job_counter
+
+from django.shortcuts import render, get_object_or_404
 
 from job.models import Job
 
 
 # Create your views here.
 def job_list(request):
-  pass
-  # jobs = Job.objects.all()
+  job_list=Job.objects.all()
+  # print(job_list) ==> for debugging
+  #must return 3 entities/objects
+  return render(request, 'job/job_list.html', {'jobs':job_list})
 
 
 def job_detail(request, job_id):
-  pass
+  # job = get_object_or_404(Job,pk=job_id) # this is the context
+  # or
+  context = {'job' : get_object_or_404(Job, pk=job_id)}
+  return render(request, 'job/job_detail.html',context)
